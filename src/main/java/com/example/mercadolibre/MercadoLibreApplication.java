364d820f93e1e91831ce7ef770d9b7badc15ff16
Jaime Andres Alberto Olarte Fuentes <jaime.olarte@global66.com>
2022-03-24 20:54:41 -0500

    Configuración para intellig community

--- src/main/java/com/example/mercadolibre/MercadoLibreApplication.java
+++ src/main/java/com/example/mercadolibre/MercadoLibreApplication.java
@@ -2,8 +2,9 @@ package com.example.mercadolibre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MercadoLibreApplication {
 
     public static void main(String[] args) {
